fd6b57b5191de60c75992fe73d6bc48302c01a39
joeysab <josephsabbagh@live.com>
2016-04-14 22:21:12 +0000

    Addressbook challenge DONE!

--- addressbook.js
+++ addressbook.js
@@ -1,7 +1,30 @@
-function createPerson (firstName, lastName){
-    return function Person (){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emails = [];
-    };
+function checkType(type) {
+  if (type === "home") {
+    return "home";
+  }
+  else if (type === "work") {
+    return "work";
+  }
+  else {
+    return "other";
+  }
 }
+
+function Person (firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emails = [];
+    };
+
+
+Person.prototype = {
+    addEmail: function (address, type){
+        this.emails.push(address + " (" + checkType(type))
+    }
+    
+}
+
+var person1 = new Person("Joseph","Sabbagh");
+person1.addEmail("jossabb@noemail.com","work")
+
+console.log(person1)
