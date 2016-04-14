1bbcc736aa91e744d9c74d2b721a34dc5c12cd8c
joeysab <josephsabbagh@live.com>
2016-04-14 20:18:30 +0000

    Question 4 Done

--- functions.js
+++ functions.js
@@ -16,4 +16,83 @@ function sortingArr(str1,str2, str3, str4){
     
 }
 
-console.log(sortingArr("hey", "nononononoo", "hihihihihi", "dakjb"))
+//console.log(sortingArr("hey", "nononononoo", "hihihihihi", "dakjb"))
+
+//Question 2
+var arr = [
+    {
+        name: "Ziad",
+        email: "ziad@noemail.com"
+        
+    },
+    {
+       name: "joseph",
+       email: "joseph@noemail.com"
+    }
+    ];
+    
+    var arrName =[]
+    for(var i = 0; i < arr.length; i++){
+        arrName.push(arr[i].name)
+    };
+    
+var resultName = arrName.sort(function(a,b){
+    if (a.length< b.length){
+        return 1;
+        
+    }else if (a.name < b.name) {
+        return -1;
+        
+    } else {
+        return 0;
+    }
+});
+
+
+var arrEmail = []
+for (var i = 0; i < arr.length; i++){
+    arrEmail.push(arr[i].email)
+};
+
+arrEmail.sort(function (a,b){
+        if (a > b){
+        return 1;
+        
+    }else if (a < b) {
+        return -1;
+        
+    } else {
+        return 0;
+    }
+});
+
+
+//console.log(arrName);
+//console.log(arrEmail)
+
+//Question 3
+function squareRoot(num){
+   var result = num.map(function(num){
+       return num * num;
+   });
+   return result;
+}
+console.log(squareRoot([25]));
+
+//Question 4
+
+var obj1 = {
+    name: "joe",
+    num: [25]
+};
+
+function objectSquare (obj){
+    var result = obj.num.map(function(num){
+       return num * num;
+    })
+    return result;
+}
+
+console.log(objectSquare(obj1))
+
+//Question 5
