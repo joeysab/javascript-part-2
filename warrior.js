326585aaadbabba57264f0b15a6e00444656cf0c
joeysab <josephsabbagh@live.com>
2016-04-16 23:44:57 +0000

    Used the prototype instead

--- warrior.js
+++ warrior.js
@@ -3,14 +3,14 @@ function randBetween(a, b) {
 }
 
 function fight(character1, character2){
-    console.log(character1.name + " rushes to the arena with " + character2.name);
+    return (character1.name + " rushes to the arena with " + character2.name);
 }
 
 function faceOff(opponent) {
     if ( this.power > opponent.power){
-        console.log('I killed you charmout!');
+        return('I killed you charmout!');
     } else {
-        console.log("OUCH! I'm dying.....");
+        return("OUCH! I'm dying.....");
     }
 }
 function Warrior (name, gender) {
@@ -19,12 +19,13 @@ function Warrior (name, gender) {
     this.level = 1;
     this.weapon = "wooden sword";
     this.power = randBetween(1,100);
-    this.fight = fight;
-    this.faceOff = faceOff;
 }
+
+Warrior.prototype.faceOff = faceOff;
+Warrior.prototype.fight = fight;
 
 var warrior1 = new Warrior("Jay", "M");
 var warrior2 = new Warrior("Mike", "M");
 
-console.log(fight(warrior1, warrior2));
 console.log(warrior1.faceOff(warrior2));
+console.log(warrior2.faceOff(warrior1));
